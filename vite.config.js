37ab3e8b85af066b17408941b68850443ccb3c3e
Chiragsinh-3 <147172549+Chiragsinh-3@users.noreply.github.com>
2025-01-20 13:00:30 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
 
-export default defineConfig({
+export default {
   plugins: [react()],
+  base: "./",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+};
